5aa9d122fa5f800d05765be0caa8decd6c82c63c
Батин Сергей <Hoshimin707@hotmail.com>
2021-03-29 17:42:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace l1p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Today:d}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
