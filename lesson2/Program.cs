6b319fd543fe1b0e120e8ab3a233a2ac225aa018
Сергей <saygon707@gmail.com>
2021-04-02 18:47:09 +0300

    добавил задание 6

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -10,11 +10,11 @@ namespace l2p
             //l2p2.Mounth();
             //l2p3.EvenNumber();
             //
-            //l2p4.Print_fiscal();
-            //l2p4.Print_line_product("Матрасы", 1209, 10, 20);
             //l2p4_2.Fiscal();
 
             //l2p5.MounthTemp();
+
+            l2p6.list();
 
 
 
---
+++ lesson2/l2p6.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l2p
+{
+    class l2p6
+    {
+
+
+        public static void list()
+        {
+            string day = "";
+            
+            int[] week = new int[] {
+            0b1000000,
+            0b0100000,
+            0b0010000,
+            0b0001000,
+            0b0000100,
+            0b0000010,
+            0b0000001,
+        };
+
+            for(int i = 0; i < week.Length; i++)
+            {
+                switch (week[i])
+                {
+                    case 0b1000000: 
+                        day = "Пн";
+                            break;
+                    case 0b0100000: 
+                        day = "Вт";
+                        break;
+                    case 0b0010000: 
+                        day = "Ср";
+                        break;
+                    case 0b0001000:
+                        day = "Чт";
+                        break;
+                    case 0b0000100:
+                        day = "Пт";
+                        break;
+                    case 0b0000010:
+                        day = "Сб";
+                        break;
+                    case 0b0000001:
+                        day = "Вс";
+                        break;
+                    default: day = "Праздник";
+                        break;
+                }
+
+                if((week[i] | 0b0111110) == 0b0111110)
+                {
+                    day += " 10.00 - 20.00";
+                } else
+                {
+                    day += " Выходной";
+                }
+
+                Console.WriteLine(day);
+
+            }
+
+        }
+
+    }
+}
