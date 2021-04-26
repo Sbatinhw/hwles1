f534f9a01f664b14c0feb8cffbc0f3db4917481a
Сергей <saygon707@gmail.com>
2021-04-26 20:48:48 +0300

    задание 8

---
+++ lesson8/l8p/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using lesson8library;
+
+namespace l8p
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            helloClass user = new helloClass();
+            user.PrintHello();
+
+            helloClass custom = new helloClass("Андрей", "Пивет..");
+            custom.PrintHello();
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lesson8/lesson8library/helloClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson8library
+{
+    public class helloClass
+    {
+        string Name { get; }
+        string HelloType { get; }
+        public helloClass()
+        {
+            Name = "Пользователь";
+            HelloType = "Здравствуйте,";
+        }
+
+        public helloClass(string name, string hello)
+        {
+            Name = name;
+            HelloType = hello;
+        }
+
+        public void PrintHello()
+        {
+            Console.WriteLine($"{HelloType} {Name}");
+        }
+
+    }
+}
